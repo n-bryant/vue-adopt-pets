f67fdd924a8660fb6b0d9cc54b674aa3a6975142
Nicholas Bryant <nbryant.developer@gmail.com>
2019-06-15 10:44:12 -0400

    feat(initial config): setup router and data files
    relates config

---
+++ src/data/dogs.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Hillary',
+    breed: 'mut',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'Super Friendly.'
+  },
+  {
+    name: 'Zeus',
+    breed: 'afghan hound',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weight: 68,
+    location: 'threed',
+    notes: 'Loves pets.'
+  },
+  {
+    name: 'Katie',
+    breed: 'golden retriever',
+    gender: 'female',
+    age: 2,
+    color: 'yellow',
+    weight: 44,
+    location: 'fourside',
+    notes: 'Really playful!'
+  }
+]
