eece7a802b0b308d271eb10a1aa370bd9fd25bd3
Nicholas Bryant <nbryant.developer@gmail.com>
2019-06-15 11:37:01 -0400

    feat(store): setup initial state for store
    relates store_setup

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -3,19 +3,22 @@
     <h1>Cats for Adoption</h1>
     <b-table striped hover :items="cats">
       <template slot="name" slot-scope="data">
-        <router-link :to="`/pets/${data.id}`">{{ data.value }}</router-link>
+        <router-link :to="`/pets/cats/${data.index}`">{{ data.value }}</router-link>
       </template>
     </b-table>
   </div>
 </template>
 
 <script>
-import cats from '@/data/cats'
+import { mapState } from 'vuex'
 export default {
   data () {
-    return {
-      cats
-    }
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
   }
 }
 </script>
--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    Pet
+    <h1>{{ pet.name }} ({{ $route.params.type }})</h1>
+    <p>Age: {{ pet.age }}</p>
+    <p>Breed: {{ pet.breed }}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      pet: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const pet = this[this.$route.params.type][this.$route.params.id]
+    this.pet = pet
+  }
+}
+</script>
