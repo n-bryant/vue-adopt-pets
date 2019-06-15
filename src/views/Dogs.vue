11e5e8e8a28ffa6a130a1aa65d90c0a57331384f
Nicholas Bryant <nbryant.developer@gmail.com>
2019-06-15 11:09:16 -0400

    feat(routing): setup routing to individual pet pages
    relates routing

--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>Dogs for Adoption</h1>
-    <b-table striped hover :items="dogs"></b-table>
+    <b-table striped hover :items="dogs">
+      <template slot="name" slot-scope="data">
+        <router-link :to="`/pets/${data.id}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
